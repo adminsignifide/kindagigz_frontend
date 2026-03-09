00e3c7cce9b8ae3ad38e71d75eecd0ad2352da2a
Caleb Karimi <karimicaleb@gmail.com>
2026-03-09 16:33:17 +0300

    Add SOCIALS routes for social medoa links that are used in the Footer

--- lib/constants/routes.ts
+++ lib/constants/routes.ts
@@ -15,7 +15,7 @@ export const ROUTES = {
   SKILLS_BOOST: '/skills-boost',
   LOGIN: '/login',
   SIGNUP: '/signup',
-  UPGRADE_TO_SERVICE_PROVIDER: '/upgrade-to-service-provider',
+  UPGRADE_TO_SERVICE_PROVIDER: '/upgrade-to-service-provider',             
 } as const;
 
 // Navigation links configuration
@@ -41,3 +41,11 @@ export const NAV_LINKS: NavLink[] = [
     public: true,
   },
 ] as const;
+
+export const SOCIALS = {
+  FACEBOOK: 'https://www.facebook.com/share/18b29zaizi/?mibextid=wwXIfr',
+  TWITTER: 'https://x.com/__kinda_?s=11&t=dj9QV_j076ekCceMsMF0XQ',
+  TIKTOK: 'https://www.tiktok.com/@__kinda._?_t=ZM-8t8VxoUt13V&_r=1',
+  LINKEDIN: 'https://www.linkedin.com/company/signifide-group-international/',
+  INSTAGRAM: 'https://www.instagram.com/__.kinda_/',              
+} as const;
